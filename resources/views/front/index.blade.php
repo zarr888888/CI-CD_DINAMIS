e55ad3d6aa4797bc250653c8fc312e8bdb94d643
zarr888888 <alfajarislamiakbar@gmail.com>
2026-02-20 14:58:46 +0700

    Add heading for CI/CD deployment success message

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -1,3 +1,4 @@
+<h1 style="text-align: center; color: red; font-size: 30px; margin-top: 20px; margin-bottom: 20px;">DEPLOY OTOMATIS CI/CD ALFAJAR SUKSES BOLOOO</h1>
 <x-blog-layout>
     <!-- Posts Section -->
     <section class="w-full lg:w-2/3 flex flex-col">
