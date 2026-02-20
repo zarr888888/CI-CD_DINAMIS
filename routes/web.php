4d93c812a611fed5702b33eadd354c537b072249
zarr888888 <alfajarislamiakbar@gmail.com>
2026-02-20 15:36:47 +0700

    Add testing route for CI/CD deployment message
    Added a route for testing CI/CD deployment with a styled message.

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,11 @@
 <?php
+Route::get('/testing-cicd', function () {
+    return '<div style="background-color: yellow; padding: 50px; text-align: center;">
+                <h1 style="color: red; font-size: 40px; font-weight: bold;">
+                     DEPLOY OTOMATIS CI/CD ALFAJAR SUKSES 100% BOLO!
+                </h1>
+            </div>';
+});
 
 /*
 |--------------------------------------------------------------------------
@@ -16,10 +23,3 @@ require __DIR__ . '/front.php';
 require __DIR__ . '/admin.php';
 require __DIR__ . '/auth.php';
 
-Route::get('/testing-cicd', function () {
-    return '<div style="background-color: yellow; padding: 50px; text-align: center;">
-                <h1 style="color: red; font-size: 40px; font-weight: bold;">
-                     DEPLOY OTOMATIS CI/CD ALFAJAR SUKSES 100% BOLO!
-                </h1>
-            </div>';
-});
